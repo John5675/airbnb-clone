a3a0b2228e19df4a240a625f88e8f1b418c7ab53
jhuynh <79059047+John5675@users.noreply.github.com>
2024-08-13 22:27:50 -0700

    Completed Account Page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="flex justify-between">
       {/* Logo */}
-      <a href="" className="flex items-center gap-1">
+      <Link to={"/"} className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -23,7 +23,7 @@ export default function Header() {
           />
         </svg>
         <span className="font-bold text-xl">airbnb</span>
-      </a>
+      </Link>
       {/* Search */}
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
@@ -50,7 +50,7 @@ export default function Header() {
       </div>
       {/* Menu */}
       <Link
-        to={"/login"}
+        to={user ? "/account" : "/login"}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
       >
         <svg
