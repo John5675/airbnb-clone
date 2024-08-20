4ce17451d3a41f6fc3fed45dd2c8ed3732729382
jhuynh <79059047+John5675@users.noreply.github.com>
2024-08-20 01:59:47 -0700

    Code Refactoring

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,215 +1,32 @@
-import { Link, useParams } from "react-router-dom";
-import { useState } from "react";
-import Perks from "../Perks";
-import axios from "axios";
+import { Link } from "react-router-dom";
+import AccountNav from "../AccountNav";
 
 export default function PlacesPage() {
-  const { action } = useParams();
-  const [title, setTitle] = useState("");
-  const [address, setAddress] = useState("");
-  const [addedPhotos, setAddedPhotos] = useState([]);
-  const [photoLink, setPhotoLink] = useState("");
-  const [description, setDescription] = useState("");
-  const [perks, setPerks] = useState([]);
-  const [extraInfo, setExtraInfo] = useState("");
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
-  const [maxGuests, setMaxGuests] = useState(1);
-
-  function inputHeader(text) {
-    return <h2 className="text-2xl mt-4">{text}</h2>;
-  }
-
-  function inputDescription(text) {
-    return <p className="text-gray-500 text-sm">{text}</p>;
-  }
-
-  function preInput(header, description) {
-    return (
-      <>
-        {inputHeader(header)}
-        {inputDescription(description)}
-      </>
-    );
-  }
-
-  async function addPhotoByLink(e) {
-    e.preventDefault();
-    const { data: filename } = await axios.post("/upload-by-link", {
-      link: photoLink,
-    });
-    setAddedPhotos((prev) => {
-      return [...prev, filename];
-    });
-    setPhotoLink("");
-  }
-
-  function uploadPhoto(e) {
-    const files = e.target.files;
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      data.append("photos", files[i]);
-    }
-    data.append("photos", files);
-    axios
-      .post("/upload", data, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        const { data: filenames } = res;
-        setAddedPhotos((prev) => {
-          return [...prev, ...filenames];
-        });
-      });
-  }
-
   return (
     <div>
-      {action !== "new" && (
-        <div className="text-center">
-          <Link
-            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-            to={"/account/places/new"}
+      <AccountNav />
+      <div className="text-center">
+        list of all added places
+        <br />
+        <Link
+          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+          to={"/account/places/new"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="size-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            Add new place
-          </Link>
-        </div>
-      )}
-      {action === "new" && (
-        <div>
-          <form>
-            {preInput(
-              "Title",
-              "Title for your place. Should be short and catchy as in advertisement"
-            )}
-            <input
-              type="text"
-              placeholder="title, for example: My lovely apt"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+            <path
+              fillRule="evenodd"
+              d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+              clipRule="evenodd"
             />
-            {preInput("Address", "Address to this place")}
-            <input
-              type="text"
-              placeholder="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-            {preInput("Photos", "The more the better")}
-            <div className="flex gap-2">
-              <input
-                type="text"
-                placeholder={"Add using a link..."}
-                value={photoLink}
-                onChange={(e) => setPhotoLink(e.target.value)}
-              />
-              <button
-                className="bg-gray-200 px-4 rounded-2xl"
-                onClick={addPhotoByLink}
-              >
-                Add&nbsp; photo
-              </button>
-            </div>
-
-            <div className="mt-2 grid gap-2 grid-cols-3 lg:grid-cols-6 md:grid-cols-4">
-              {addedPhotos.length > 0 &&
-                addedPhotos.map((link, index) => (
-                  <div className="h-32 flex" key={index}>
-                    <img
-                      className="rounded-2xl w-full object-cover"
-                      src={"http://localhost:4000/uploads/" + link}
-                      alt=""
-                    />
-                  </div>
-                ))}
-              {console.log(addedPhotos.length)}
-
-              <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                <input type="file" className="hidden" onChange={uploadPhoto} />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
-                  />
-                </svg>
-                Upload
-              </label>
-            </div>
-            {preInput("Description", "Describe your place")}
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            {preInput("Perks", "select all the perks of your place")}
-            <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-              <Perks selected={perks} onChange={setPerks} />
-            </div>
-
-            {preInput("Extra info", "house rules, etc")}
-            <textarea
-              value={extraInfo}
-              onChange={(e) => setExtraInfo(e.target.value)}
-            />
-            {preInput(
-              "Check in&out times",
-              "add check in and out times, remember to have some time window for cleaning the room between guests"
-            )}
-            <div className="grid gap-2 sm:grid-cols-3">
-              <div>
-                <h3 className="mt-2 mb-1">Check in time</h3>
-                <input
-                  type="text"
-                  placeholder="14"
-                  value={checkIn}
-                  onChange={(e) => setCheckIn(e.target.value)}
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 mb-1">Check out time</h3>
-                <input
-                  type="text"
-                  placeholder="11"
-                  value={checkOut}
-                  onChange={(e) => setCheckOut(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <h3 className="mt-2 -mb-1">Max number of guests</h3>
-                <input
-                  type="number"
-                  value={maxGuests}
-                  onChange={(e) => setMaxGuests(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <button className="primary my-4">Save</button>
-          </form>
-        </div>
-      )}
+          </svg>
+          Add new place
+        </Link>
+      </div>
     </div>
   );
 }
